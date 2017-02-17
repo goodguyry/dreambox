ea5024a4221311717781dac8f1a6435373157f77
Ryan Domingue <ryan@alleyinteractive.com>
2017-02-16 19:01:11 -0800

    Removes unused .deb files to save disk space

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -16,5 +16,19 @@ echo "Removing temporary files"
 rm -rf /tmp/files
 rm -rf /tmp/packages
 
+declare -a PACKAGES=(
+  '/usr/local/src/httpd_2.2.31-1_amd64.deb'
+  '/usr/local/src/imagemagick-6.6.9_10-1_amd64.deb'
+  '/usr/local/src/imagick_3.2.0RC1-1_amd64.deb'
+  '/usr/local/src/mod-fastcgi_2.4.6-1_amd64.deb'
+  '/usr/local/src/mysql_5.5.40-1_amd64.deb'
+);
+
+for INDEX in ${!PACKAGES[*]}; do
+  if [[ -r "${PACKAGES[$INDEX]}" ]]; then
+    sudo rm -f "${PACKAGES[$INDEX]}"
+  fi
+done
+
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
