85f5af652f3da8314c78b1e8380c87b1c6a73400
Ryan Domingue <ryan@alleyinteractive.com>
2017-05-21 22:02:43 -0700

    Updates SSL setup and host collection

--- templates/config-setup.rb
+++ templates/config-setup.rb
@@ -17,7 +17,7 @@ module Config
   vagrant_dir = File.expand_path(Dir.pwd)
 
   # Build the config filepath
-  if defined? vm_config_file
+  if defined?(vm_config_file)
     vm_config_file_path = File.join(vagrant_dir, vm_config_file)
   else
     vm_config_file_path = File.join(vagrant_dir, 'vm-config.yml')
@@ -28,8 +28,7 @@ module Config
   if File.file?(vm_config_file_path) then
     VM_CONFIG = YAML.load_file(vm_config_file_path)
   else
-    puts "Config file '#{vm_config_file_path}' not found.".bold.red
-    abort "===> See 'Getting Started': https://github.com/goodguyry/dreambox/wiki".bold.yellow
+    print_error "Config file '#{vm_config_file_path}' not found."
   end
 
   VM_CONFIG['hosts'] = Array.new
@@ -62,24 +61,45 @@ module Config
       items = Hash.new
     end
 
-    # Establish defaults
+    # Establish site defaults
     defaults = Hash.new
-    defaults['local_root'] = 'web'
+    defaults['local_root'] = 'www'
+    defaults['box_name'] = VM_CONFIG['name']
+
+    # Set the site host value
+    if ! defined?(items['host']) || ! (items['host'].kind_of? String) then
+      if (items['hosts'].kind_of? Array) && 0 < items['hosts'].length then
+        # Add first `hosts` value for the `host` property
+        items['host'] = items['hosts'][0]
+      else
+        print_error "Missing `host` and/or `hosts` values."
+      end
+    end
 
     # Build paths here rather than in a provisioner
     items['root_path'] = "/home/#{items['username']}/#{items['web_root']}"
-    items['vhost_file'] = "/usr/local/apache2/conf/vhosts/#{items['hosts'][0]}.conf"
+    items['vhost_file'] = "/usr/local/apache2/conf/vhosts/#{items['host']}.conf"
 
-    # Temporary fix
-    if items['hosts'].kind_of? Array then
-      items['host'] = items['hosts'][0]
-    end
-
-    # Take hosts and create a large string in a root 'hosts' property
-    # just push it, since it doesn't matter what type it is
-    if (items['ssl'] || VM_CONFIG['box']['ssl']) then
+    # If SSL is enabled globally and not disabled locally, or if enabled locally
+    if (VM_CONFIG['ssl'] && (false != items['ssl'] || ! defined?(items['ssl']))) || items['ssl'] then
+      # Enable the root SSL setting if not already enabled
       VM_CONFIG['ssl_enabled'] = true
-      VM_CONFIG['hosts'] = VM_CONFIG['hosts'].push(*items['hosts'])
+      # Ensure the site SSL setting is enabled
+      # If it's enabled globally, but not at the site, ssl_setup will fail
+      items['ssl'] = true
+      # Add the site's main host to the root `hosts` property
+      if defined?(items['host']) then
+        VM_CONFIG['hosts'] = VM_CONFIG['hosts'].push(*items['host'])
+      end
+      # Add each of the site's hosts to the root 'hosts' property
+      if items['hosts'].kind_of? Array then
+        items['hosts'].each do |host|
+          # De-dup hosts values
+          if ! VM_CONFIG['hosts'].include?(host) then
+            VM_CONFIG['hosts'] = VM_CONFIG['hosts'].push(*host)
+          end
+        end
+      end
     end
 
     # Delete properties we no longer need
@@ -92,21 +112,11 @@ module Config
   # Merge hosts into string in a root 'hosts' property
   VM_CONFIG['hosts'] = VM_CONFIG['hosts'].join(',')
 
-  # Allowed php values
-  php_versions = ['5', '7']
-  php_dirs = ['php56', 'php70']
-
-  # Set default 'box' values
-  box_defaults = Hash.new
-  box_defaults['name'] = 'dreambox'
-  box_defaults['php'] = php_versions[0]
-
-  # Merge the default 'box' values with those from vm-config
-  VM_CONFIG['box'] = box_defaults.merge(VM_CONFIG['box'])
-
-  # Abort of the php version isn't one of the two specific options
-  if ! php_versions.include?(VM_CONFIG['box']['php']) then
-    abort "===> Dreambox config: Acceptable `php` values are '#{php_versions[0]}' and '#{php_versions[1]}'".bold.red
+  # One last check to make sure we have hosts
+  # If not, force disable SSL
+  if 1 > VM_CONFIG['hosts'].length then
+    VM_CONFIG['ssl_enabled'] = false
+    puts "===> Dreambox config: Missing hosts list; SSL disabled.".bold.yellow
   end
 
   if VM_CONFIG['debug'] then
@@ -126,7 +136,7 @@ module Config
       puts "     #{site}: web_root     #{items['web_root']}".bold
       puts "     #{site}: local_root   #{items['local_root']}".bold
       if 0 < "#{items['ssl']}".length then
-	puts "     #{site}: ssl          #{items['ssl']}".bold
+        puts "     #{site}: ssl          #{items['ssl']}".bold
       end
       puts "     #{site}: box_name     #{items['box_name']}".bold
       puts "     #{site}: host         #{items['host']}".bold
