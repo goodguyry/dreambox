e834bae73f97fd80e279b85e466fed22b9b7f181
Ryan Domingue <ryan@alleyinteractive.com>
2017-05-30 09:44:30 -0700

    Adds box value tests

--- tests/run.rb
+++ tests/run.rb
@@ -8,8 +8,27 @@ vm_config = Config::VM_CONFIG
 @failing = []
 @passing = []
 
-expect_type('box.name', vm_config['name'], String)
-expect_value('box.name', vm_config['name'], 'dreambox-tests')
+# Type tests
+# expect_type('box.name', vm_config['name'], String)
+
+# Value tests
+expect_value(
+  'box.php_dir',
+  vm_config['php_dir'],
+  'php70'
+)
+
+expect_value(
+  'box.ssl_enabled',
+  vm_config['ssl_enabled'],
+  true
+)
+
+expect_value(
+  'box.hosts',
+  vm_config['hosts'],
+  'www.dreambox.test,dreambox.test,app.dreambox.test,example.dev,help.example.dev'
+)
 
 # Test stats output
 print_stats
