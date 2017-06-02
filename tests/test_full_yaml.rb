a02e2c23f5814f8f4a8e70a4ebf3305b24f68e00
Ryan Domingue <ryan@alleyinteractive.com>
2017-06-02 13:04:12 -0700

    Incorporates Tests class into full yaml tests

---
+++ tests/test_full_yaml.rb
@@ -0,0 +1,196 @@
+full_config_file = 'tests/test_full.yaml'
+full_dns_hosts_file = File.join(File.dirname(__FILE__), 'test_full_yaml_dns_hosts.txt')
+testing = Config.new(full_config_file, full_dns_hosts_file)
+
+@tests.assertions.push(*[
+  # Gobal Test
+  #
+  # PHP Directory
+  # `php_dir`
+  {
+    'name' => 'box.php_dir',
+    'expect' => 'php70',
+    'actual' => testing.config['php_dir']
+  },
+
+  # Gobal Test
+  #
+  # Global SSL setting
+  # `ssl_enabled`
+  {
+    'name' => 'box.ssl_enabled',
+    'expect' => true,
+    'actual' => testing.config['ssl_enabled']
+  },
+
+  # Gobal Test
+  #
+  # hosts list
+  {
+    'name' => 'box.hosts',
+    'expect' => 'www.fullconfig.dev,fullconfig.dev,app.fullconfig.dev,minimalconfig.dev,help.minimalconfig.dev',
+    'actual' => testing.config['hosts']
+  },
+
+  # Site Test
+  #
+  # Root Path
+  # With public folder
+  {
+    'name' => 'sites.full.root_path',
+    'expect' => '/home/fc_user/fullconfig.com/public',
+    'actual' => testing.config['sites']['full']['root_path']
+  },
+
+  # Site Test
+  #
+  # Root Path
+  # No public folder
+  {
+    'name' => 'sites.minimalconfig.root_path',
+    'expect' => '/home/mc_user/minimalconfig.com',
+    'actual' => testing.config['sites']['minimal']['root_path']
+  },
+
+  # Site Test
+  #
+  # Root Path - Subdomain
+  # With public folder
+  {
+    'name' => 'sites.app.full.root_path',
+    'expect' => '/home/fc_user/fullconfig.com/public/app',
+    'actual' => testing.config['sites']['app.full']['root_path']
+  },
+
+  # Site Test
+  #
+  # Root Path - Subdomain
+  # No public folder
+  {
+    'name' => 'sites.help.minimal.root_path',
+    'expect' => '/home/mc_user/minimalconfig.com/app/help',
+    'actual' => testing.config['sites']['help.minimal']['root_path']
+  },
+
+  # Site Test
+  #
+  # VHost File
+  {
+    'name' => 'sites.full.vhost_file',
+    'expect' => '/usr/local/apache2/conf/vhosts/full.conf',
+    'actual' => testing.config['sites']['full']['vhost_file']
+  },
+
+  # Site Test
+  #
+  # VHost File - Subdomain
+  {
+    'name' => 'sites.app.full.vhost_file',
+    'expect' => '/usr/local/apache2/conf/vhosts/app.full.conf',
+    'actual' => testing.config['sites']['app.full']['vhost_file']
+  },
+
+  # Site Test
+  #
+  # Subdomain Host
+  # Parent has bare domain
+  {
+    'name' => 'sites.help.minimal.host',
+    'expect' => 'help.minimalconfig.dev',
+    'actual' => testing.config['sites']['help.minimal']['host']
+  },
+
+  # Site Test
+  #
+  # Subdomain Host
+  # Parent host contains `www.`
+  {
+    'name' => 'sites.app.full.host',
+    'expect' => 'app.fullconfig.dev',
+    'actual' => testing.config['sites']['app.full']['host']
+  },
+
+  # Site Test
+  #
+  # Subdomain SSL
+  # Parent has SSL enabled
+  {
+    'name' => 'sites.app.full.ssl',
+    'expect' => true,
+    'actual' => testing.config['sites']['app.full']['ssl']
+  },
+
+  # Site Test
+  #
+  # Subdomain SSL
+  # Parent has SSL disabled
+  {
+    'name' => 'sites.help.minimal.ssl',
+    'expect' => false,
+    'actual' => testing.config['sites']['help.minimal']['ssl']
+  },
+
+  # Global Test
+  #
+  # DNS Hosts file
+  #
+  {
+    'name' => 'DNS Hosts file',
+    'expect' => File.open(full_dns_hosts_file, 'r').read.split("\n"),
+    'actual' => [
+      'DNS.1 = www.fullconfig.dev',
+      'DNS.2 = fullconfig.dev',
+      'DNS.3 = app.fullconfig.dev',
+      'DNS.4 = minimalconfig.dev',
+      'DNS.5 = help.minimalconfig.dev'
+    ]
+  },
+
+  # Utility function
+  #
+  # trim_slashes
+  {
+    'name' => 'trim_slashes',
+    'expect' => 'path/to/directory',
+    'actual' => trim_slashes('/path/to/directory/')
+  },
+
+  # Utility function
+  #
+  # trim_slashes
+  # Path has no beginning or ending slash
+  {
+    'name' => 'trim_slashes - no slashes',
+    'expect' => 'path/to/directory',
+    'actual' => trim_slashes('path/to/directory')
+  },
+
+  # Utility function
+  #
+  # trim_ending_slash
+  {
+    'name' => 'trim_ending_slash',
+    'expect' => '/path/to/directory',
+    'actual' => trim_ending_slash('/path/to/directory/')
+  },
+
+  # Utility function
+  #
+  # trim_ending_slash
+  # Path has no ending slash
+  {
+    'name' => 'trim_ending_slash - no ending slash',
+    'expect' => '/path/to/directory',
+    'actual' => trim_ending_slash('/path/to/directory')
+  },
+
+  # Utility function
+  #
+  # trim_beginning_slash
+  # Path has no beginning slash
+  {
+    'name' => 'trim_beginning_slash - no beginning slash',
+    'expect' => 'path/to/directory/',
+    'actual' => trim_beginning_slash('path/to/directory/')
+  },
+])
