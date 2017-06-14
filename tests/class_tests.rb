814b5cd5bbf1460fced10923c5aaa980398ae162
Ryan Domingue <ryan@alleyinteractive.com>
2017-06-14 00:51:33 -0700

    Fixes print_stats output

--- tests/class_tests.rb
+++ tests/class_tests.rb
@@ -31,9 +31,9 @@ class Tests
     if @failing.length > 0
       puts "==> #{@failing.length}/#{@tests_run} tests failed\n".bold.red
       @failing.each do |message|
-        puts "#{message.first}\n"
-        printf "Expected  => %s\n".yellow, message.first
-        printf "Actual    => %s\n\n".red, message.last
+        puts "#{message[:name]}\n"
+        printf "Expected  => %s\n".yellow, message[:expected]
+        printf "Actual    => %s\n\n".red, message[:actual]
       end
     end
   end
@@ -62,7 +62,7 @@ class Tests
       if condition_met
         @passing.push("`#{test['name']}` value")
       else
-        message = test['name'], test['expect'], test['actual']
+        message = { name: test['name'], expected: test['expect'], actual: test['actual'] }
         @failing.push(message)
       end
       @tests_run += 1
