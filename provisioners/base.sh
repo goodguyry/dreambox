592828c170284ad502b324bafbdbca97faf813ab
Ryan Domingue <ryan@alleyinteractive.com>
2018-01-31 22:32:10 -0800

    Moves temporary Apache setup to Apache provisioner

--- provisioners/base.sh
+++ provisioners/base.sh
@@ -192,52 +192,6 @@ done
 # SSL config
 cp /tmp/files/ssl/dreambox-openssl.cnf /usr/lib/ssl/dreambox-openssl.cnf
 
-# Apache setup
-
-# Create VHosts directory
-[[ ! -d /dh/apache2/template/etc/extra/vhosts ]] && mkdir /dh/apache2/template/etc/extra/vhosts
-# cp /tmp/files/http/httpd.conf /usr/local/apache2/conf/
-# cp /tmp/files/http/ports.conf /dh/apache2/template/etc/extra/vhosts/
-
-echo "Replacing strings in configuration files"
-
-# Change httpd2 init script to use /bin/bash
-sed -i -r 's/(#! )(\/bin\/sh)/\1 \/bin\/bash/' /etc/init.d/httpd2;
-
-# Load mod_fcgid
-sed -i -r '/LoadModule rewrite_module.*\.so/a LoadModule fcgid_module lib/modules/mod_fcgid.so' \
-  /dh/apache2/template/etc/httpd.conf;
-# Include the VHost directory
-sed -i -r 's/(#)(Include etc\/extra\/)httpd-vhosts\.conf/\2vhosts\/\*/' \
-  /dh/apache2/template/etc/httpd.conf;
-
-# Duplicate template
-cp -r /dh/apache2/template /dh/apache2/apache2-dreambox;
-# Move httpd file to template root
-ln -s /dh/apache2/apache2-dreambox/sbin/httpd /dh/apache2/apache2-dreambox/apache2-dreambox-httpd;
-# Change the config paths
-sed -i -r 's/(\/usr\/local\/dh\/apache2\/)(template)/\1apache2-dreambox/' \
-  /dh/apache2/apache2-dreambox/etc/httpd.conf;
-# Set the PID path
-sed -i -r '/ServerRoot \"\/usr\/local\/dh\/apache2\//a PidFile "/var/run/apache2-dreambox-httpd.pid"' \
-  /dh/apache2/apache2-dreambox/etc/httpd.conf;
-
-# Create the logs directory
-# @TODO This could likely change to whatever... it just needs to match what's in ndn-vhost.conf
-mkdir -p /home/_domain_logs/vagrant/dreambox.http/http.dreambox-instance;
-# Copy the test vhost.conf into place
-# @TODO Once this is finalized, move to template with placeholder strings
-cp /vagrant/ndn-vhost.conf /dh/apache2/apache2-dreambox/etc/extra/vhosts/;
-# Create test domain path
-# @TODO Remove after testing
-mkdir -p /home/vagrant/dreambox.http;
-# Add a PHP info test page
-# @TODO Remove after testing
-bash -c "echo '<?php phpinfo(); ?>' >> /home/vagrant/dreambox.http/index.php";
-
-# Start Apache
-/etc/init.d/httpd2 start;
-
 # # Remove existing motd and set up ours
 rm -f /etc/update-motd.d/*
 for MOTD in /tmp/files/motd/*; do
--- provisioners/setup.apache.sh
+++ provisioners/setup.apache.sh
@@ -3,6 +3,47 @@
 # Post-install Apache setup.
 #
 
+# Create VHosts directory
+[[ ! -d /dh/apache2/template/etc/extra/vhosts ]] && mkdir /dh/apache2/template/etc/extra/vhosts
+# cp /tmp/files/http/httpd.conf /usr/local/apache2/conf/
+# cp /tmp/files/http/ports.conf /dh/apache2/template/etc/extra/vhosts/
+
+echo "Replacing strings in configuration files"
+
+# Change httpd2 init script to use /bin/bash
+sed -i -r 's/(#! )(\/bin\/sh)/\1 \/bin\/bash/' /etc/init.d/httpd2;
+
+# Load mod_fcgid
+sed -i -r '/LoadModule rewrite_module.*\.so/a LoadModule fcgid_module lib/modules/mod_fcgid.so' \
+  /dh/apache2/template/etc/httpd.conf;
+# Include the VHost directory
+sed -i -r 's/(#)(Include etc\/extra\/)httpd-vhosts\.conf/\2vhosts\/\*/' \
+  /dh/apache2/template/etc/httpd.conf;
+
+# Duplicate template
+cp -r /dh/apache2/template /dh/apache2/apache2-dreambox;
+# Move httpd file to template root
+ln -s /dh/apache2/apache2-dreambox/sbin/httpd /dh/apache2/apache2-dreambox/apache2-dreambox-httpd;
+# Change the config paths
+sed -i -r 's/(\/usr\/local\/dh\/apache2\/)(template)/\1apache2-dreambox/' \
+  /dh/apache2/apache2-dreambox/etc/httpd.conf;
+# Set the PID path
+sed -i -r '/ServerRoot \"\/usr\/local\/dh\/apache2\//a PidFile "/var/run/apache2-dreambox-httpd.pid"' \
+  /dh/apache2/apache2-dreambox/etc/httpd.conf;
+
+# Create the logs directory
+# @TODO This could likely change to whatever... it just needs to match what's in ndn-vhost.conf
+mkdir -p /home/_domain_logs/vagrant/dreambox.http/http.dreambox-instance;
+# Copy the test vhost.conf into place
+# @TODO Once this is finalized, move to template with placeholder strings
+cp /vagrant/ndn-vhost.conf /dh/apache2/apache2-dreambox/etc/extra/vhosts/;
+# Create test domain path
+# @TODO Remove after testing
+mkdir -p /home/vagrant/dreambox.http;
+# Add a PHP info test page
+# @TODO Remove after testing
+bash -c "echo '<?php phpinfo(); ?>' >> /home/vagrant/dreambox.http/index.php";
+
 echo 'Finishing Apache install'
 
 # Set Apache to start at boot
@@ -36,6 +77,5 @@ for DIRECTORY in ${DIRECTORIES[*]}; do
   mkdir ${DIRECTORY}
 done
 
-echo "Starting Apache"
-/etc/init.d/apache2 start
-# /etc/init.d/apache2: line 15: /usr/local/apache2/bin/apachectl: No such file or directory
+# Start Apache
+/etc/init.d/httpd2 start;
