55656ed4693867ea55b2fcfdea0a6d51c1b47fbc
Ryan Domingue <ryan@alleyinteractive.com>
2018-02-23 23:43:32 -0800

    Updates tests and test configs for PHP updates

--- tests/assertions/php.rb
+++ tests/assertions/php.rb
@@ -7,52 +7,64 @@
 
   ## ===> Default (Missing)
 
-  # Condition;
+  # Condition:
   # - The root `php` property is not declared.
   #
   # Expected Outcome:
-  # - The root `php` value should default to '5'.
+  # - The root `php` value should default to '70'.
   {
-    'name' => "Missing PHP: The root `php` value should default to '5'.",
-    'expect' => '5',
+    'name' => "Missing PHP: The root `php` value should default to '70'.",
+    'expect' => '70',
     'actual' => @tests.the['required'].config['php'],
   },
 
-  # Condition;
+  # Condition:
   # - The root `php` property is not declared.
   #
   # Expected Outcome:
-  # - The root `php_dir` property created by Config should default to 'php56'.
+  # - The root `php_dir` property created by Config should default to 'php70'.
   {
-    'name' => "Missing PHP: The root `php_dir` property should default to 'php56'.",
-    'expect' => 'php56',
+    'name' => "Missing PHP: The root `php_dir` property should default to 'php70'.",
+    'expect' => 'php70',
     'actual' => @tests.the['required'].config['php_dir'],
   },
 
-  ## ===> PHP 5
+  ## ===> PHP 71
 
-  # Condition;
-  # - The root `php` value is '5'.
+  # Condition:
+  # - The root `php` value is '71'.
   #
   # Expected Outcome:
   # - The root `php_dir` value should match the config version.
   {
-    'name' => "PHP 5: The root `php_dir` value should match the config version.",
-    'expect' => 'php56',
-    'actual' => @tests.the['typical'].config['php_dir'],
+    'name' => "PHP 71: The root `php_dir` value should match the config version.",
+    'expect' => 'php71',
+    'actual' => @tests.the['full'].config['php_dir'],
   },
 
-  ## ===> PHP 7
+  ## ===> Inherited or Overridden
 
-  # Condition;
-  # - The root `php` value is '7'.
+  # Conditions:
+  # - The root `php` value is '71'.
+  # - The site `php` value is '56'.
   #
   # Expected Outcome:
-  # - The root `php_dir` value should match the config version.
+  # - The site's `php_dir` value should match the site's version.
   {
-    'name' => "PHP 7: The root `php_dir` value should match the config version.",
+    'name' => "PHP 56: The site's `php_dir` value should override the root value.",
+    'expect' => 'php56',
+    'actual' => @tests.the['full'].config['sites']['full-one']['php_dir'],
+  },
+
+  # Condition:
+  # - The root `php` property is not declared.
+  #
+  # Expected Outcome:
+  # - The site `php_dir` property should inherit the default version from the root.
+  {
+    'name' => "PHP 70: The site's `php_dir` value should override the root value.",
     'expect' => 'php70',
-    'actual' => @tests.the['full'].config['php_dir'],
+    'actual' => @tests.the['required'].config['sites']['required']['php_dir'],
   },
 
 ])
